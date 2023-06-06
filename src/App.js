2024f448cc79962430091af8215e9c29b67951a3
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2023-06-06 13:41:07 +0900

    complete contact section in draft

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./header/Header";
 import Hero from "./hero/Hero";
 import Skills from "./skills/Skills";
 import Portfolio from "./portfolio/Portfolio";
+import Contact from "./contact/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Skills />
       <Portfolio />
+      <Contact />
     </Fragment>
   );
 }
---
+++ src/contact/Contact.js
@@ -0,0 +1,22 @@
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1 className="contact_title">Hire me for your next amazing project</h1>
+      <div className="contact_text">
+        <p className="contact_text-top">
+          Let's make something new, different and more meaningful!
+        </p>
+        <p className="contact_text_bottom">
+          {" "}
+          I'm always excited to hear new ideas, or even just a friendly
+          <span>'Hello!'</span>
+        </p>
+      </div>
+      <div className="hero_btn">
+        <p>Hire Me!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;

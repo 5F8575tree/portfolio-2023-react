d5cca2a99efc39a52208850120d5c240a5d2bfb2
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2023-06-06 21:05:50 +0900

    complete basic fade in for the top part of skills box

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,42 +1,21 @@
 import React, { useEffect, useRef } from "react";
 
 function Skills() {
-  const iconObserver = useRef(null);
-  const textObserver = useRef(null);
+  const iconsRef = useRef([]);
 
   useEffect(() => {
-    iconObserver.current = new IntersectionObserver((entries) => {
+    const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add("show");
-          iconObserver.current.unobserve(entry.target);
         }
       });
     });
 
-    const hiddenIcons = document.querySelectorAll(".hiddenIcon");
-    hiddenIcons.forEach((el) => iconObserver.current.observe(el));
+    iconsRef.current.forEach((el) => observer.observe(el));
 
     return () => {
-      iconObserver.current.disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
-    textObserver.current = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("showText");
-          textObserver.current.unobserve(entry.target);
-        }
-      });
-    });
-
-    const hiddenTexts = document.querySelectorAll(".hiddenText");
-    hiddenTexts.forEach((el) => textObserver.current.observe(el));
-
-    return () => {
-      textObserver.current.disconnect();
+      observer.disconnect();
     };
   }, []);
 
@@ -44,39 +23,48 @@ function Skills() {
     <div className="skills">
       <div className="skills_title">Skills & Influences</div>
       <div className="skills_box">
-        <div className="skills_box_prog--top">
+        <div
+          className="skills_box_prog--top hidden"
+          ref={(el) => (iconsRef.current[0] = el)}
+        >
           <img
             src="../../prog-portfolio.png"
             alt="computer programming icon"
-            className="skills_icon hiddenIcon"
+            className="skills_icon"
           />
-          <h2 className="skills_box_title hiddenText">Programming</h2>
+          <h2 className="skills_box_title">Programming</h2>
         </div>
         <div className="skills_line"></div>
         <div className="skills_box_prog--bottom">
           <p>Ruby, Rails, JavaScript, TypeScript, React, HTML/CSS</p>
         </div>
         <div className="skills_line"></div>
-        <div className="skills_box_design--top">
+        <div
+          className="skills_box_design--top hidden"
+          ref={(el) => (iconsRef.current[1] = el)}
+        >
           <img
             src="../../design-icon-portfolio.png"
             alt="design icon"
-            className="skills_icon hiddenIcon"
+            className="skills_icon"
           />
-          <h2 className="skills_box_title hiddenText">Design</h2>
+          <h2 className="skills_box_title">Design</h2>
         </div>
         <div className="skills_line"></div>
         <div className="skills_box_design--bottom">
           <p>Figma, Slack, Github, Microsoft Suite, Adobe</p>
         </div>
         <div className="skills_line"></div>
-        <div className="skills_box_influences--top">
+        <div
+          className="skills_box_influences--top hidden"
+          ref={(el) => (iconsRef.current[2] = el)}
+        >
           <img
             src="../../influences-icon-portfolio.png"
             alt="influences icon"
-            className="skills_icon hiddenIcon"
+            className="skills_icon"
           />
-          <h2 className="skills_box_title hiddenText">Influences</h2>
+          <h2 className="skills_box_title">Influences</h2>
         </div>
         <div className="skills_line"></div>
         <div className="skills_box_influences--bottom">

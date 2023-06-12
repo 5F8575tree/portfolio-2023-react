9eadd695c72a3b40477b9e8abad56b6e8561bfa5
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2023-06-12 15:48:03 +0900

    add a VERY basic fade in keyframes animation to the card component

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -1,29 +1,93 @@
+import { useState } from "react";
 import Card from "../card/Card";
 
 function Portfolio() {
+  const [selectedOption, setSelectedOption] = useState("All");
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+  };
+
+  const renderCards = () => {
+    if (selectedOption === "All") {
+      return (
+        <>
+          <Card
+            title="ii-Vent"
+            image="../../daniela-mota-TxZXttbojmA-unsplash.jpeg"
+          />
+          <Card
+            title="MyReads"
+            image="../../Cassette Tape - pexels-markus-spiske-92083.png"
+          />
+          <Card
+            title="Books B'n'B"
+            image="../../milena-trifonova-pHqt1DsHCx0-unsplash.jpeg"
+          />
+        </>
+      );
+    } else if (selectedOption === "Ruby on Rails") {
+      return (
+        <>
+          <Card
+            title="ii-Vent"
+            image="../../daniela-mota-TxZXttbojmA-unsplash.jpeg"
+          />
+          <Card
+            title="Books B'n'B"
+            image="../../milena-trifonova-pHqt1DsHCx0-unsplash.jpeg"
+          />
+        </>
+      );
+    } else if (selectedOption === "ReactJS") {
+      return (
+        <Card
+          title="MyReads"
+          image="../../Cassette Tape - pexels-markus-spiske-92083.png"
+        />
+      );
+    } else if (selectedOption === "Design Concepts") {
+      return (
+        <Card
+          title="Design Concepts Project"
+          image="../../design-concepts-project.jpg"
+        />
+      );
+    }
+  };
+
   return (
     <div className="portfolio">
       <h1 className="portfolio_title">Portfolio</h1>
       <div className="portfolio_nav">
         <ul>
-          <li>All</li>
-          <li>Ruby on Rails</li>
-          <li>ReactJS</li>
-          <li>Design Concepts</li>
+          <li
+            className={selectedOption === "All" ? "selected" : ""}
+            onClick={() => handleOptionClick("All")}
+          >
+            All
+          </li>
+          <li
+            className={selectedOption === "Ruby on Rails" ? "selected" : ""}
+            onClick={() => handleOptionClick("Ruby on Rails")}
+          >
+            Ruby on Rails
+          </li>
+          <li
+            className={selectedOption === "ReactJS" ? "selected" : ""}
+            onClick={() => handleOptionClick("ReactJS")}
+          >
+            ReactJS
+          </li>
+          <li
+            className={selectedOption === "Design Concepts" ? "selected" : ""}
+            onClick={() => handleOptionClick("Design Concepts")}
+          >
+            Design Concepts
+          </li>
         </ul>
       </div>
-      <Card
-        title="ii-Vent"
-        image="../../daniela-mota-TxZXttbojmA-unsplash.jpeg"
-      />
-      <Card
-        title="MyReads"
-        image="../../Cassette Tape - pexels-markus-spiske-92083.png"
-      />
-      <Card
-        title="Books B'n'B"
-        image="../../milena-trifonova-pHqt1DsHCx0-unsplash.jpeg"
-      />
+      {renderCards()}
     </div>
   );
 }
